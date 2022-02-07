a79653c345ea494f52433787f27a8813a01e5e49
lucas <mr.pine97@gmail.com>
2022-02-07 20:21:10 -0300

    req1 - refact: acrescenta o await e refatora para tratar erros

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,13 @@
 const userService = require('../services/user');
 
 const create = async (req, res) => {
-  const { displayName, email, password, image } = req.body;
-  const newUser = userService.create({ displayName, email, password, image });
-
-  return res.status(201).json(newUser);
+  try {
+    const { displayName, email, password, image } = req.body;
+    await userService.create({ displayName, email, password, image });
+    return res.status(201).json({ message: 'User successfully registered' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
