81ddcdeb2373c070967629ec7888ae8d33dac28d
lucas <mr.pine97@gmail.com>
2022-02-07 18:52:54 -0300

    req1 - feat: cria o router

--- router/users.js
+++ router/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const userController = require('../controllers/user');
+const isEmailInTheBody = require('../middlewares/user/isEmailInTheBody');
+const isEmailUnique = require('../middlewares/user/isEmailUnique');
+const isEmailValid = require('../middlewares/user/isEmailValid');
+const isNameSizeValid = require('../middlewares/user/isNameSizeValid');
+const isPasswordInTheBody = require('../middlewares/user/isPasswordInTheBody');
+const isPasswordValid = require('../middlewares/user/isPasswordValid');
 
 const router = express.Router();
 
-router.post('/', userController.create);
+router.post('/',
+  isNameSizeValid, 
+  isEmailInTheBody, 
+  isEmailValid, 
+  isPasswordInTheBody, 
+  isPasswordValid, 
+  isEmailUnique,
+  userController.create);
 
 module.exports = router;
