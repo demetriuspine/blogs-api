828fdaf32f415db973fce3d91ef83d8c24a5e18f
lucas <mr.pine97@gmail.com>
2022-02-08 16:22:11 -0300

    req2 - feat: inclui os middlewares na rota POST login

--- router/login.js
+++ router/login.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const loginController = require('../controllers/login');
+const isEmailEmpty = require('../middlewares/login/isEmailEmpty');
+const isPasswordEmpty = require('../middlewares/login/isPasswordEmpty');
 const { verifyEmailAndPassword } = require('../middlewares/login/verifyEmailAndPassword');
+const isEmailInTheBody = require('../middlewares/user/isEmailInTheBody');
+const isPasswordInTheBody = require('../middlewares/user/isPasswordInTheBody');
 
 const router = express.Router();
 
-router.post('/', verifyEmailAndPassword, loginController.login);
+router.post('/',
+  isEmailInTheBody,
+  isPasswordInTheBody,
+  isEmailEmpty,
+  isPasswordEmpty,
+  verifyEmailAndPassword,
+  loginController.login);
 
 module.exports = router;
