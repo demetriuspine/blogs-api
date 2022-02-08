99d85fce3a1a2894b1334abe86426e73f3a4085d
lucas <mr.pine97@gmail.com>
2022-02-08 17:19:29 -0300

    req3 - feat: adiciona o middleware de verificaçao do token

---
+++ middlewares/auth/jwtValidation.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { User } = require('../../models');
+
+const jwtValidation = async (req, res, next) => { // função adaptada do conteudo do course
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ where: { id: decoded.data.id } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (e) {
+    console.error(e);
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = jwtValidation;
