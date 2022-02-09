822f440d1065b21c4a849b45265e0e46c868d200
lucas <mr.pine97@gmail.com>
2022-02-09 14:33:31 -0300

    req6 - feat: adiciona controller getAll

--- controllers/category.js
+++ controllers/category.js
@@ -10,6 +10,16 @@ const create = async (req, res) => {
   }
 };
 
+const getAll = async (_req, res) => {
+  try {
+    const categories = await categoryService.getAll();
+    return res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
