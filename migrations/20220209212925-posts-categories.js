a8e88d58e5fc3823645da04d6227e721b23a0018
lucas <mr.pine97@gmail.com>
2022-02-09 18:52:25 -0300

    req7 - feat: adiciona as instancias e cria um model para postsCategories

---
+++ migrations/20220209212925-posts-categories.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable('PostsCategories', {
+      postId: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'BlogPosts', key: 'id'},
+        type: Sequelize.INTEGER,
+      },
+      categoryId: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'Category', key: 'id'},
+        type: Sequelize.INTEGER,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
