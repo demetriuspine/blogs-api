36b124fced4f38f4eeaef38c338d015e743e071c
lucas <mr.pine97@gmail.com>
2022-02-09 14:33:18 -0300

    req6 - feat: adiciona service getAll

--- services/category.js
+++ services/category.js
@@ -5,6 +5,12 @@ const create = async ({ name }) => {
   return newUser;
 };
 
+const getAll = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
+
 module.exports = {
   create,
+  getAll,
 };
