10794c77e9d22e75034a3418fdcbea6a298d892a
lucas <mr.pine97@gmail.com>
2022-02-09 12:50:17 -0300

    req5 - refact: corrige a resposta

--- controllers/category.js
+++ controllers/category.js
@@ -3,8 +3,8 @@ const categoryService = require('../services/category');
 const create = async (req, res) => {
   try {
     const { name } = req.body;
-    await categoryService.create({ name });
-    return res.status(201).json({ message: 'Category successfully registered' });
+    const categoryName = await categoryService.create({ name });
+    return res.status(201).json(categoryName);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
