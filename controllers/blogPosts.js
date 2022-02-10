b3e5c1f16480134f03b3c13b347a2c4fa96e6793
lucas <mr.pine97@gmail.com>
2022-02-10 13:43:31 -0300

    req8 - feat: adiciona o controller getall

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -11,6 +11,16 @@ const create = async (req, res) => {
   }
 };
 
+const getAll = async (_req, res) => {
+  try {
+    const blogPosts = await BlogPostsService.getAll();
+    return res.status(200).json(blogPosts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
