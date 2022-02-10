1c5281d5695fad5ac124cc4a9d4ff68d37e691bf
lucas <mr.pine97@gmail.com>
2022-02-10 13:08:51 -0300

    req7 - feat: insere os middlewares no router

--- router/blogPosts.js
+++ router/blogPosts.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const blogPostsController = require('../controllers/blogPosts');
 const jwtValidation = require('../middlewares/auth/jwtValidation');
+const { isTheBodyComplete } = require('../middlewares/blogPosts/isTheBodyComplete');
+const verifyCategoryExistence = require('../middlewares/blogPosts/verifyCategoryExistence');
 
 const router = express.Router();
 
-router.post('/', jwtValidation, blogPostsController.create);
+router.post('/',
+jwtValidation, isTheBodyComplete, verifyCategoryExistence, blogPostsController.create);
 
 module.exports = router;
